3de725a74a0a742fed8253a80ebfc80b4fbb4881
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-11-29 12:47:13 -0700

    javascript logic finished for moving, have to link it to socket io for interactivity

--- public/js/gameroom.js
+++ public/js/gameroom.js
@@ -41,27 +41,41 @@ $(document).ready(function() {
 var checked;
 
 $(".chessBlock").on("click", function (){
-    
-    if ($(this).attr("style") === "border: 5px solid yellow;"){
-    $(this).attr("style", "")
-    }
-    else{
-        // Holy shit it worked
-        $(this).attr("style", "border: 5px solid yellow;");
-       
-        if (checked === $(this)){
-            
-            $(this).attr("style", "border: 5px solid yellow;");
-
+    if(turn===side){
+        if($(this).attr("status") === opposingSide && move[0] === undefined){
+            return
         }
-        else if (checked){
-            checked.attr("style","");
-            checked = $(this);
+        if($(this).attr("status")==="error"){
+            return
+        }
+        if (checked === $(this)){
+            console.log("returned")
+                
+            return
+    
+         }
+    
+        if ($(this).attr("style") === "border: 5px solid yellow;"){
+        $(this).attr("style", "")
         }
         else{
-            checked= $(this);
-        }
-      }
+            // Holy shit it worked
+            $(this).attr("style", "border: 5px solid yellow;");
+           
+          
+            if (checked){
+                checked.attr("style","");
+                checked = $(this);
+            }
+            else{
+                checked= $(this);
+            }
+          }
+
+    }
+    else{}
+
+    
 })
 
 var message;
@@ -69,10 +83,15 @@ var message;
 $(".submit").on("click", event=>{
     event.preventDefault();
     message = $("#chatInput").val();
+    if(message && message !== ""){
     $("#chatInput").val("");
-    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; padding-left: 10px; border-radius: 15px; background-color: grey; text-align: left; font-size: 24px; border: 3px solid yellow;").text("You: "+ message));
+    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; margin-right: 5px; margin-left: 5px; padding-left: 10px; border-radius: 15px; background-color: grey; text-align: left; font-size: 24px; border: 3px solid yellow;").text("You: "+ message));
     console.log("click");
     socket.emit("send-message", message);
+    message = "";
+    }
+    else{}
+
 })
 
 
@@ -93,10 +112,109 @@ socket.on("main-chat", data=>{
 
 function appendNewMessage(message){
 
-    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; padding-right: 10px; border-radius: 15px; background-color: rgb(34, 194, 223); color: grey;  word-break: break-all; white-space: normal; text-align: right; font-size: 24px; border: 3px solid yellow;").text(message));
+    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; padding-right: 10px; margin-right: 5px; margin-left: 5px; border-radius: 15px; background-color: rgb(34, 194, 223); color: grey;  word-break: break-all; white-space: normal; text-align: right; font-size: 24px; border: 3px solid yellow;").text(message));
   
 }
 
 function flasher(){
     $("#newUser").attr("class", "flash");
 }
+
+
+//PIECE MOVER
+var turn = "white";
+var side = "white";
+var opposingSide = "black";
+var move = [];
+var piece;
+var moving = false;
+var pieceSelected = false;
+var moveSend="";
+var oldSpace;
+//testing only
+var switcher;
+
+
+$(".chessBlock").on("click", function () {
+    if(turn != side){
+
+
+    }
+    else{
+        if( pieceSelected===true && move[1] === undefined && $(this).attr("status") != "empty" && $(this).attr("status") != opposingSide){
+            console.log("new piece selected to move")
+            move[0]=$(this).attr("value");
+            console.log(move);
+            piece =$(this).attr("id");
+            console.log(piece);
+            moving = true;
+            oldSpace = $(this);
+        }
+
+        if(side === $(this).attr("status")){
+            console.log("your piece");
+           
+
+            if(pieceSelected===false){
+            move.push($(this).attr("value"));
+            piece =$(this).attr("id");
+            oldSpace = $(this);
+            pieceSelected = true;
+            moving = true;
+
+            console.log(piece);
+            console.log(move);
+                       console.log("first piece selected")
+           
+            }
+        }
+        if(pieceSelected===true && moving === true && move[1] === undefined){
+            if($(this).attr("status") === "empty"){
+                move.push($(this).attr("value"));
+                console.log(`${piece} piece was moved to ${move[1]}`);
+                $(this).attr("status", side);
+                $(this).attr("id", piece)
+                $(this).attr("style", "");
+                oldSpace.attr("status", "empty").attr("id", "");
+                oldSpace = "";  
+                move=[];
+                pieceSelected = false;
+                moving = false;
+                turn=opposingSide;
+                    //testing purposes
+                    switcher = opposingSide;
+                    console.log("TCL: switcher = opposingSide;", switcher, opposingSide)
+                    opposingSide = side;
+                    side =  switcher;
+                    switcher = "";
+                   
+
+            }
+           
+
+            if($(this).attr("status") === opposingSide && moving === true){
+                attackedPiece = $(this).attr("id");
+                console.log(`${piece} attacked ${attackedPiece}  ` )
+                move.push($(this).attr("value"));
+                console.log(`${piece} piece was moved to ${move[1]}`);
+                $(this).attr("status", side);
+                $(this).attr("id", piece)
+                $(this).attr("style", "");
+                oldSpace.attr("status", "empty").attr("id", "");
+                oldSpace = "";
+                move=[]; 
+                pieceSelected = false;
+                moving = false;
+                turn=opposingSide;
+                //testing purposes
+                switcher = opposingSide;
+                opposingSide = side;
+                side =  switcher;
+                switcher = "";
+            }
+        }
+
+        
+    }
+})
+
