f74176b14655e2b77f82bd11a559bbd529b82f85
Jessie Jones <Jessie.Jones.Jr@gmail.com>
2019-11-29 20:00:00 -0700

    working but with serious bugs

--- public/js/gameroom.js
+++ public/js/gameroom.js
@@ -1,8 +1,93 @@
 
 
 $(document).ready(function() {
+    var socket = io("http://localhost:3001");
+    var side;
+    var sideChoosen = false;
+ 
+    $(document).on("click", "#blackside", function(){
+        if(sideChoosen === true){
+            return
+        }
+        else{
+            side ="black";
+            $(this).text("You");
+            $("#whiteside").text("Waiting..");
+            $("#chooseSideBanner").text("Waiting on opponent...")
+            socket.emit("side-send", side);
+            oppossingSideFunc();
+            sideChoosen = true;
+        }
 
+     
+        
+    })
+    $(document).on("click", "#whiteside", function(){
+        if(sideChoosen === true){
+            return
+        }
+        else{
+            $(this).text("You");
+            $("#blackside").text("Waiting..");
+            $("#chooseSideBanner").text("Waiting on opponent...")
+            side ="white";
+            socket.emit("side-send", side);
+            oppossingSideFunc();
+            sideChoosen = true;
+        }
+  
 
+        
+    })
+
+    socket.on("side-checker", data=>{
+        var opponentHolder;
+        if(data === "white_taken"){
+            sideChoosen = true;
+            side ="black";
+            oppossingSideFunc();
+            var jQvarUser = "#"+side+"side";
+            var jQvarOpponent = "#"+oppossingSide+"side";
+            $(jQvarUser).text("You");
+            $(jQvarOpponent).text("Opponent");
+            $("#chooseSideBanner").text("Opponent took white ...")
+           
+            setTimeout(function(){ 
+                socket.emit("side-send", side); 
+            }, 3000);
+            
+           
+            
+        }
+
+        if(data ==="black_taken"){
+            sideChoosen = true;
+            side ="white";
+            oppossingSideFunc();
+            var jQvarUser = "#"+side+"side";
+            var jQvarOpponent = "#"+oppossingSide+"side";
+            $(jQvarUser).text("You");
+            $(jQvarOpponent).text("Opponent");
+            $("#chooseSideBanner").text("Opponent took white ...");
+     
+            setTimeout(function(){ 
+                socket.emit("side-send", side); 
+            }, 3000);
+            
+            
+
+        }
+        if(data === "start"){
+            $("#chooseSideBanner").attr("style", "display: none;");
+            $("#chessboardContainer").attr("style","opacity: 1");
+            var jQvarOpponent = "#"+oppossingSide+"side";
+            $(jQvarOpponent).text("Opponent");
+            
+            
+
+        }
+
+    })
     const player = localStorage.getItem("player");
 
     // modal
@@ -35,6 +120,188 @@ $(document).ready(function() {
       $("#newUser").attr("class", "");
     }
   }
+
+
+var message;
+
+$(".submit").on("click", event=>{
+    event.preventDefault();
+    message = $("#chatInput").val();
+
+
+    if(message && message !== ""){
+    $("#chatInput").val("");
+    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; margin-right: 5px; margin-left: 5px; padding-left: 10px; border-radius: 15px; background-color: grey; text-align: left; font-size: 24px; border: 3px solid yellow;").text("You: "+ message));
+    console.log("click");
+
+    socket.emit("send-message", message);
+    message = "";
+    }
+    else{}
+
+})
+
+
+
+
+socket.on("main-chat", data=>{
+    console.log(data)
+    appendNewMessage(data);
+    flasher();
+})
+
+
+function appendNewMessage(message){
+
+    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; padding-right: 10px; margin-right: 5px; margin-left: 5px; border-radius: 15px; background-color: rgb(34, 194, 223); color: grey;  word-break: break-all; white-space: normal; text-align: right; font-size: 24px; border: 3px solid yellow;").text(message));
+  
+}
+
+function flasher(){
+    $("#newUser").attr("class", "flash");
+}
+
+
+//PIECE MOVER
+var turn = "black";
+
+var oppossingSide;
+function oppossingSideFunc(){
+    if(side ==="black") {
+        oppossingSide = "white"
+    }
+    else {
+        oppossingSide="black";
+    }
+    
+    
+};
+
+var move = [];
+var piece;
+var moving = false;
+var pieceSelected = false;
+var moveSend=[];
+var oldSpace;
+//testing only
+var switcher;
+
+
+$(".chessBlock").on("click", function () {
+   
+    if(turn === oppossingSide){
+
+
+    }
+    else{
+        if( pieceSelected===true && move[1] === undefined && $(this).attr("status") != "empty" && $(this).attr("status") != oppossingSide){
+            oldSpace = $(this);
+            console.log(oldSpace)
+            console.log("new piece selected to move")
+            move[0]=$(this).attr("value");
+            console.log(move);
+            piece =$(this).attr("id");
+            console.log(piece);
+            moving = true;
+            
+        }
+
+        if(side === $(this).attr("status")){
+            console.log("your piece");
+           
+
+            if(pieceSelected===false){
+            oldSpace = $(this);
+            console.log(oldSpace)
+            move.push($(this).attr("value"));
+            piece =$(this).attr("id");
+            
+            pieceSelected = true;
+            moving = true;
+
+            console.log(piece);
+            console.log(move);
+                       console.log("first piece selected")
+           
+            }
+        }
+        if(pieceSelected===true && moving === true && move[1] === undefined){
+
+            if($(this).attr("status") === "empty"){
+                move.push($(this).attr("value"));
+                console.log(`${piece} piece was moved to ${move[1]}`);
+
+                $(this).attr("status", side);
+                $(this).attr("id", piece)
+                $(this).attr("style", "");
+
+                moveSend.push(oldSpace);
+                moveSend.push(move);
+                console.log(moveSend);
+                socket.emit("send-move", moveSend);
+                
+                oldSpace.attr("status", "empty");
+                oldSpace.attr("id", "");
+                oldSpace.attr("style","");
+             
+               
+                
+                oldSpace = "";  
+                move=[];
+                pieceSelected = false;
+                moving = false;
+                moveSend = [];
+                turn=oppossingSide;
+
+                    //testing purposes
+                    // switcher = opposingSide;
+                    // console.log("TCL: switcher = opposingSide;", switcher, opposingSide)
+                    // opposingSide = side;
+                    // side =  switcher;
+                    // switcher = "";
+                   
+
+            }
+           
+
+            if($(this).attr("status") === oppossingSide && moving === true){
+                attackedPiece = $(this).attr("id");
+                console.log(`${piece} attacked ${attackedPiece}  ` )
+                move.push($(this).attr("value"));
+                console.log(`${piece} piece was moved to ${move[1]}`);
+
+                $(this).attr("status", side);
+                $(this).attr("id", piece)
+                $(this).attr("style", "");
+
+                moveSend.push(oldSpace);
+                moveSend.push(move);
+                console.log(moveSend)
+                socket.emit("send-move", moveSend);
+
+                oldSpace.attr("status", "empty");
+                oldSpace.attr("id", "");
+                oldSpace.attr("style","");
+             
+                
+                oldSpace = "";
+                move=[]; 
+                pieceSelected = false;
+                moving = false;
+                moveSend = [];
+                turn=oppossingSide;
+                //testing purposes
+                // switcher = opposingSide;
+                // opposingSide = side;
+                // side =  switcher;
+                // switcher = "";
+            }
+        }
+
+        
+    }
+})
+
 //sudo need logic that grabs player 1 or player 2 status such that you can subscribe to the chat name as a variable
 // I need logic that also discriminates localhost/game/gameID:PORT as the serving host for the chat.
 
@@ -42,7 +309,7 @@ var checked;
 
 $(".chessBlock").on("click", function (){
     if(turn===side){
-        if($(this).attr("status") === opposingSide && move[0] === undefined){
+        if($(this).attr("status") === oppossingSide && move[0] === undefined){
             return
         }
         if($(this).attr("status")==="error"){
@@ -78,143 +345,30 @@ $(".chessBlock").on("click", function (){
     
 })
 
-var message;
+socket.on("move", data =>{
+    console.log(data);
+    var target = (data[1])[0];
+    console.log("TCL: target", target)
+    var final = (data[1])[1]
+    console.log("TCL:  final",  final)
+    var piece = $("."+target).attr("id");
+    var status = $("."+target).attr("status")
+    console.log(piece)
+    $("."+final).attr("id", piece).attr("status", status);
+    $("."+target).attr("id", "").attr("status", "empty");
+    var final = "";
+    var target = "";
+    turn=side;
 
-$(".submit").on("click", event=>{
-    event.preventDefault();
-    message = $("#chatInput").val();
-    if(message && message !== ""){
-    $("#chatInput").val("");
-    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; margin-right: 5px; margin-left: 5px; padding-left: 10px; border-radius: 15px; background-color: grey; text-align: left; font-size: 24px; border: 3px solid yellow;").text("You: "+ message));
-    console.log("click");
-    socket.emit("send-message", message);
-    message = "";
-    }
-    else{}
-
-})
-
-
-const socket = io("http://localhost:3001");
-
-socket.on("main-chat", data=>{
-    
-    appendNewMessage(data);
-    flasher();
+    console.log(turn)
 
 })
 
-// socket.on(player, data=>{
-//     console.log(data)
-// })
+socket.on(player, data=>{
+    console.log(data)
+})
 
 });
 
-function appendNewMessage(message){
 
-    $(".chatBox").append($("<p>").attr("style", "margin-top: 5px; padding-right: 10px; margin-right: 5px; margin-left: 5px; border-radius: 15px; background-color: rgb(34, 194, 223); color: grey;  word-break: break-all; white-space: normal; text-align: right; font-size: 24px; border: 3px solid yellow;").text(message));
-  
-}
-
-function flasher(){
-    $("#newUser").attr("class", "flash");
-}
-
-
-//PIECE MOVER
-var turn = "white";
-var side = "white";
-var opposingSide = "black";
-var move = [];
-var piece;
-var moving = false;
-var pieceSelected = false;
-var moveSend="";
-var oldSpace;
-//testing only
-var switcher;
-
-
-$(".chessBlock").on("click", function () {
-    if(turn != side){
-
-
-    }
-    else{
-        if( pieceSelected===true && move[1] === undefined && $(this).attr("status") != "empty" && $(this).attr("status") != opposingSide){
-            console.log("new piece selected to move")
-            move[0]=$(this).attr("value");
-            console.log(move);
-            piece =$(this).attr("id");
-            console.log(piece);
-            moving = true;
-            oldSpace = $(this);
-        }
-
-        if(side === $(this).attr("status")){
-            console.log("your piece");
-           
-
-            if(pieceSelected===false){
-            move.push($(this).attr("value"));
-            piece =$(this).attr("id");
-            oldSpace = $(this);
-            pieceSelected = true;
-            moving = true;
-
-            console.log(piece);
-            console.log(move);
-                       console.log("first piece selected")
-           
-            }
-        }
-        if(pieceSelected===true && moving === true && move[1] === undefined){
-            if($(this).attr("status") === "empty"){
-                move.push($(this).attr("value"));
-                console.log(`${piece} piece was moved to ${move[1]}`);
-                $(this).attr("status", side);
-                $(this).attr("id", piece)
-                $(this).attr("style", "");
-                oldSpace.attr("status", "empty").attr("id", "");
-                oldSpace = "";  
-                move=[];
-                pieceSelected = false;
-                moving = false;
-                turn=opposingSide;
-                    //testing purposes
-                    switcher = opposingSide;
-                    console.log("TCL: switcher = opposingSide;", switcher, opposingSide)
-                    opposingSide = side;
-                    side =  switcher;
-                    switcher = "";
-                   
-
-            }
-           
-
-            if($(this).attr("status") === opposingSide && moving === true){
-                attackedPiece = $(this).attr("id");
-                console.log(`${piece} attacked ${attackedPiece}  ` )
-                move.push($(this).attr("value"));
-                console.log(`${piece} piece was moved to ${move[1]}`);
-                $(this).attr("status", side);
-                $(this).attr("id", piece)
-                $(this).attr("style", "");
-                oldSpace.attr("status", "empty").attr("id", "");
-                oldSpace = "";
-                move=[]; 
-                pieceSelected = false;
-                moving = false;
-                turn=opposingSide;
-                //testing purposes
-                switcher = opposingSide;
-                opposingSide = side;
-                side =  switcher;
-                switcher = "";
-            }
-        }
-
-        
-    }
-})
 
